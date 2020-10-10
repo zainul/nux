7bd69454b2d0885b90d1d7a40135e0414301ecc7
zainul <zainulmasadi90@gmail.com>
2020-10-10 16:44:59 +0700

    update data type numeric

--- internal/validator/numeric/validator.go
+++ internal/validator/numeric/validator.go
@@ -45,8 +45,26 @@ type validator struct {
 
 // Validate implementation of numeric validation
 func (v *validator) Validate(val interface{}) (bool, *domain.NuxError) {
-	l := val.(int)
+	if l, ok := val.(int); ok {
+		return v.validateInt(l)
+	}
 
+	if l, ok := val.(int64); ok {
+		return v.validateInt64(l)
+	}
+
+	if l, ok := val.(float64); ok {
+		return v.validateFloat64(l)
+	}
+
+	if l, ok := val.(float32); ok {
+		return v.validateFloat32(l)
+	}
+
+	return true, nil
+}
+
+func (v *validator) validateInt(l int) (bool, *domain.NuxError) {
 	if v.NonZero && l == 0 {
 		// fmt.Println("non zero detected", l, v)
 		return false, helper.FindError(v.ErrorCode[NonZero])
@@ -81,7 +99,120 @@ func (v *validator) Validate(val interface{}) (bool, *domain.NuxError) {
 		// fmt.Println("gt detected", l, v)
 		return false, helper.FindError(v.ErrorCode[GreaterThan])
 	}
+	return true, nil
+}
 
+func (v *validator) validateInt64(l int64) (bool, *domain.NuxError) {
+	if v.NonZero && l == 0 {
+		// fmt.Println("non zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[NonZero])
+	}
+
+	if v.Min != nil && l < int64(*v.Min) {
+		// fmt.Println("min detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Min])
+	}
+
+	if v.Max != nil && l > int64(*v.Max) {
+		// fmt.Println("max zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Max])
+	}
+
+	if v.LessThanEqual != nil && l <= int64(*v.LessThanEqual) {
+		// fmt.Println("lte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThanEqual])
+	}
+
+	if v.GreaterThanEqual != nil && l >= int64(*v.GreaterThanEqual) {
+		// fmt.Println("gte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThanEqual])
+	}
+
+	if v.LessThan != nil && l < int64(*v.LessThan) {
+		// fmt.Println("lt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThan])
+	}
+
+	if v.GreaterThan != nil && l > int64(*v.GreaterThan) {
+		// fmt.Println("gt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThan])
+	}
+	return true, nil
+}
+
+func (v *validator) validateFloat64(l float64) (bool, *domain.NuxError) {
+	if v.NonZero && l == 0 {
+		// fmt.Println("non zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[NonZero])
+	}
+
+	if v.Min != nil && l < float64(*v.Min) {
+		// fmt.Println("min detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Min])
+	}
+
+	if v.Max != nil && l > float64(*v.Max) {
+		// fmt.Println("max zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Max])
+	}
+
+	if v.LessThanEqual != nil && l <= float64(*v.LessThanEqual) {
+		// fmt.Println("lte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThanEqual])
+	}
+
+	if v.GreaterThanEqual != nil && l >= float64(*v.GreaterThanEqual) {
+		// fmt.Println("gte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThanEqual])
+	}
+
+	if v.LessThan != nil && l < float64(*v.LessThan) {
+		// fmt.Println("lt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThan])
+	}
+
+	if v.GreaterThan != nil && l > float64(*v.GreaterThan) {
+		// fmt.Println("gt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThan])
+	}
+	return true, nil
+}
+
+func (v *validator) validateFloat32(l float32) (bool, *domain.NuxError) {
+	if v.NonZero && l == 0 {
+		// fmt.Println("non zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[NonZero])
+	}
+
+	if v.Min != nil && l < float32(*v.Min) {
+		// fmt.Println("min detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Min])
+	}
+
+	if v.Max != nil && l > float32(*v.Max) {
+		// fmt.Println("max zero detected", l, v)
+		return false, helper.FindError(v.ErrorCode[Max])
+	}
+
+	if v.LessThanEqual != nil && l <= float32(*v.LessThanEqual) {
+		// fmt.Println("lte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThanEqual])
+	}
+
+	if v.GreaterThanEqual != nil && l >= float32(*v.GreaterThanEqual) {
+		// fmt.Println("gte detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThanEqual])
+	}
+
+	if v.LessThan != nil && l < float32(*v.LessThan) {
+		// fmt.Println("lt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[LessThan])
+	}
+
+	if v.GreaterThan != nil && l > float32(*v.GreaterThan) {
+		// fmt.Println("gt detected", l, v)
+		return false, helper.FindError(v.ErrorCode[GreaterThan])
+	}
 	return true, nil
 }
 
